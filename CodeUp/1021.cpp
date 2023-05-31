fae02e04a215310b835bd6fafa29878ecd991b68
geeneoning <wnsussid@gmail.com>
2023-05-31 23:00:28 +0900

    Add files via upload

---
+++ CodeUp/1021.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char data[51];
+
+    cin >> data;
+    cout << data;
+}
---
+++ CodeUp/1023.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n,f;
+    char dat;
+    
+    cin >> n >> dat >> f;
+    cout << n << endl << f << endl;
+};
