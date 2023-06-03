c5cada5898a3946a88096bb2c0f26e3c78ea4bf3
geeneoning <wnsussid@gmail.com>
2023-06-03 23:51:32 +0900

    Create 1026.cpp

---
+++ CodeUp/1026.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int hour, minute, second;
+  char col;
+  col = ":"
+	cin >> hour >> col;
+	cin >> minute >> col;
+	cin >> second >> col;
+
+	cout << minute << endl;
+}
